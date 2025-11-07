2fdbf4b8f04f442a0e73a9ce1ab31e1a0b912141
vijaykd04 <vijaykantd21@gmail.com>
2025-11-07 18:39:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://lms-1-z7z0.onrender.com"
 
 function App() {
   
